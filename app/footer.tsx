d36b569a170e8726c3d37e841974f23832f7c488
이규하 <progyu@github.com>
2025-08-07 12:28:02 +0900

    feat: 풋터에 RSS Feed 링크 추가
    - Twitter 링크를 RSS Feed 링크로 교체하여 콘텐츠 구독 편의성 향상
    - 사용자가 블로그 업데이트를 쉽게 구독할 수 있도록 개선

--- app/footer.tsx
+++ app/footer.tsx
@@ -4,11 +4,9 @@ export function Footer() {
   return (
     <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
       <div className="grow text-left">
-        LeeGyuHa (
-        <A target="_blank" href="https://x.com/GyuHa10">
-          @GyuHa
+        <A target="_blank" href="/atom">
+          RSS Feed
         </A>
-        )
       </div>
       <div>
         <A target="_blank" href="https://github.com/progyu/blog">
